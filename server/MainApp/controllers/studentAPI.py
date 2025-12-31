7774b1d63f0097da9435e3c8f45a79e0c6a4459e
Vinh <vinhthanhtran03102004@gmail.com>
2025-12-31 10:28:43 +0700

    [Update] Change import paths

--- server/MainApp/controllers/studentAPI.py
+++ server/MainApp/controllers/studentAPI.py
@@ -2,25 +2,25 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
-from MainApp import models as m
-from MainApp import serializers as s
+from MainApp.models import Student
+from MainApp.serializers import StudentSerializer, StudentWithIDSerializer
 
 class StudentController(APIView):
     def get(self, request):
-        students = m.Student.objects.all()
-        students_serializer = s.StudentWithIDSerializer(students,many=True)
+        students = Student.objects.all()
+        students_serializer = StudentWithIDSerializer(students,many=True)
         return Response(students_serializer.data)
     
     def post(self, request):
-        students_serializer = s.StudentSerializer(data=request.data)
+        students_serializer = StudentSerializer(data=request.data)
         if students_serializer.is_valid():
             students_serializer.save()
             return Response("Thêm học sinh vào cơ sở dữ liệu thành công!", status=status.HTTP_201_CREATED)
         return Response(students_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
     def put(self, request, sid):
-        students = m.Student.objects.get(student_id = sid)
-        students_serializer = s.StudentSerializer(students, data=request.data)
+        students = Student.objects.get(student_id = sid)
+        students_serializer = StudentSerializer(students, data=request.data)
         if students_serializer.is_valid():
             students_serializer.save()
             return Response("Cập nhật thông tin thành công!")
@@ -28,8 +28,8 @@ class StudentController(APIView):
     
     def delete(self, request, sid):
         try:
-            students=m.Student.objects.get(student_id=sid)
+            students = Student.objects.get(student_id=sid)
             students.delete()
             return Response("Xóa học sinh thành công!")
-        except m.Student.DoesNotExist:
+        except Student.DoesNotExist:
             return Response("Không tìm thấy học sinh!", status=status.HTTP_404_NOT_FOUND)
